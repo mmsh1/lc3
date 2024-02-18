fcec1cbd1a5c75994bc353d8a454e1dfbf9d0a0c
Михаил Шиманов <mihail.shimanov@bergen.tech>
2024-02-18 23:24:14 +0300

    use lookup table instead of switch

--- lc3.c
+++ lc3.c
@@ -198,26 +198,8 @@ main(int argc, char **argv)
 
 	while (lc3.running) {
 		uint16_t instr = memread(&lc3, lc3.pc++);
-		uint16_t op = instr >> 12;
-
-		switch (op) {
-		case OP_BR:   break;
-		case OP_ADD:  break;
-		case OP_LD:   break;
-		case OP_ST:   break;
-		case OP_JSR:  break;
-		case OP_AND:  break;
-		case OP_LDR:  break;
-		case OP_STR:  break;
-		case OP_RTI:  break;
-		case OP_NOT:  break;
-		case OP_LDI:  break;
-		case OP_STI:  break;
-		case OP_JMP:  break;
-		case OP_RES:  break;
-		case OP_LEA:  break;
-		case OP_TRAP: break;
-		}
+		uint16_t opc = instr >> 12;
+		optable[opc].func(&lc3);
 	}
 
 	return 0;
